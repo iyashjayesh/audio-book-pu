759c511e8eb3e757228f0b373fb46b6a4c58c66c
iyashjayesh <er.yashjayeshchauhan@gmail.com>
2022-02-20 19:59:00 +0530

    chapter 1 data added for testing

--- src/MainPages/Books/booksChapters/Book1/bch1.js
+++ src/MainPages/Books/booksChapters/Book1/bch1.js
@@ -16,11 +16,11 @@ const Book1 = () => {
             {/* <ChaptercardThree img1={book1} path1="/sutrasthana/chapter1" img2={book2} path2="/" img3={book3} path3="/"/> */}
             {/* <ChaptercardThree text1="ch1" path1="/sutrasthana/chapter1" text2="ch1" path2="/" text3="ch1" path3="/"/> */}
             <div className="audio-gallery center">
-                <SingleAudioCard sholkno="Sholk 1" sholk="अथातो दीर्घञ्जीवितीयमध्यायं व्याख्यास्यामः ||१||" audio={r1}/>
+                <SingleAudioCard sholkno="Sholk 1" sholk="अथातो दीर्घञ्जीवितीयमध्यायं व्याख्यास्यामः ||१||" audio="https://drive.google.com/file/d/1qoySGWt9CIECYYH3izaF2J7Iu0wBdQu3/view"/>
                 <SingleAudioCard sholkno="Sholk 2" sholk="इति ह स्माह भगवानात्रेयः ||२||" audio={r1} />
                 <SingleAudioCard sholkno="Sholk 3" sholk="दीर्घं जीवितमन्विच्छन्भरद्वाज उपागमत् |इन्द्रमुग्रतपा बुद्ध्वा शरण्यममरेश्वरम् ||३" audio={r1}/>
                 <SingleAudioCard sholkno="Sholk 4" sholk="ब्रह्मणा हि यथाप्रोक्तमायुर्वेदं प्रजापतिः |जग्राह निखिलेनादावश्विनौ तु पुनस्ततः ||४||" audio={r1}/>
-                <SingleAudioCard sholkno1="Sholk 5" sholk1="अश्विभ्यां भगवाञ्छक्रः प्रतिपेदे ह केवलम् |ऋषिप्रोक्तो भरद्वाजस्तस्माच्छक्रमुपागमत् ||५||" audio1={r1} audio={r1}/>
+                <SingleAudioCard sholkno="Sholk 5" sholk="अश्विभ्यां भगवाञ्छक्रः प्रतिपेदे ह केवलम् |ऋषिप्रोक्तो भरद्वाजस्तस्माच्छक्रमुपागमत् ||५||" audio={r1} audio={r1}/>
                 <SingleAudioCard sholkno="Sholk 6" sholk="विघ्नभूता यदा रोगाः प्रादुर्भूताः शरीरिणाम् |तपोपवासाध्ययनब्रह्मचर्यव्रतायुषाम् ||६||" audio={r1}/>
                 
                 {/* <AudioCard 
